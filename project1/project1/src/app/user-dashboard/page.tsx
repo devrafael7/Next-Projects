6fd9ada7cd97d0f82199ca624318b5d52affdd02
Rafael <rafarafa99944@gmail.com>
2025-06-01 19:34:20 -0300

    new charts added

--- project1/project1/src/app/user-dashboard/page.tsx
+++ project1/project1/src/app/user-dashboard/page.tsx
@@ -1,7 +1,20 @@
+import { MainCard } from "@/components/local/MainCard";
+import { Card } from "@/components/ui/card";
+import { ChartBarLabel } from "@/components/ui/chart-bar-label";
+import {  ChartLineDefault } from "@/components/ui/chart-line-default";
+
+
 export default function userDashboard(){
     return (
-        <div>
-            <h1>Hello, User!</h1>
+        <div className="w-full min-h-screen flex flex-col px-2 py-3">
+            <header className="flex gap-3">
+                <MainCard/>
+                <MainCard/>
+                <MainCard/>
+                <MainCard/>
+            </header>
+            <ChartBarLabel/>
+            <ChartLineDefault/>
         </div>
     )
 }
---
+++ project1/project1/src/components/local/MainCard.tsx
@@ -0,0 +1,10 @@
+export function MainCard(){
+    return (
+        <div className="w-full flex flex-col justify-center items-center h-max px-5 py-3 mb-2 rounded-md border border-solid border-gray-500">
+            <h1>Sales</h1>
+            <p>
+                828
+            </p>
+        </div>
+    )
+}
