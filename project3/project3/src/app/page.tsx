79571db079853e784a954ad70cad8be1b796ce14
Rafael <rafarafa99944@gmail.com>
2025-07-10 00:43:21 -0300

    creating profile editing page

---
+++ project3/project3/src/app/page.tsx
@@ -0,0 +1,84 @@
+import { TiCogOutline } from "react-icons/ti";
+import { TiArrowLeft } from "react-icons/ti";
+import { TiArrowRight } from "react-icons/ti";
+import { TiHeartOutline } from "react-icons/ti";
+import { TiFolder } from "react-icons/ti";
+import { TiLocationOutline } from "react-icons/ti";
+import { TiWorldOutline } from "react-icons/ti";
+import { TiCreditCard } from "react-icons/ti";
+import { TiTrash } from "react-icons/ti";
+import { TiTime } from "react-icons/ti";
+import { TbLogout2 } from "react-icons/tb";
+import { MdArrowForwardIos } from "react-icons/md";
+import { IoMdCheckmark } from "react-icons/io";
+
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen flex flex-col bg-gray-200 px-4 py-4 items-center">
+      <header className="w-full flex justify-between items-center mb-7">
+        <TiArrowLeft className="w-9 h-auto"/>      
+        <span className="text-lg font-bold">
+          My Profile
+        </span>
+        <TiCogOutline className="w-7 h-auto"/>
+      </header>
+
+      <div className="w-full flex h-auto items-center justify-start mb-7">
+        <article className="h-32 w-32 rounded-full bg-blue-500">
+        </article>
+        <div className="flex flex-col items-center justify-center flex-grow">
+          <h1 className="font-semibold text-xl">Rafael Oliveira</h1>
+          <h5 className="mb-4 text-gray-500">rafael@test.com</h5>
+          <button className="w-max px-5 py-1.5 rounded-md bg-blue-500 text-white" type="button">
+            Edit Profile
+          </button>
+        </div>
+      </div>
+
+      <section className="w-full flex flex-col items-center gap-2">
+        <div className="w-full flex justify-between items-center">
+          <div className="flex justify-center items-center">
+            <TiHeartOutline className="w-6 h-6 mr-4"/>
+            <span>
+              Favourites
+            </span>
+          </div>
+          <TiArrowRight className="w-8 h-8"/>
+          
+        </div>
+         <div className="w-full flex justify-between items-center">
+          <div className="flex justify-center items-center">
+            <TiHeartOutline className="w-6 h-6 mr-4"/>
+            <span>
+              Favourites
+            </span>
+          </div>
+          <TiArrowRight className="w-8 h-8"/>
+          
+        </div>
+         <div className="w-full flex justify-between items-center">
+          <div className="flex justify-center items-center">
+            <TiHeartOutline className="w-6 h-6 mr-4"/>
+            <span>
+              Favourites
+            </span>
+          </div>
+          <TiArrowRight className="w-8 h-8"/>
+          
+        </div>
+         <div className="w-full flex justify-between items-center">
+          <div className="flex justify-center items-center">
+            <TiHeartOutline className="w-6 h-6 mr-4"/>
+            <span>
+              Favourites
+            </span>
+          </div>
+          <TiArrowRight className="w-8 h-8"/>
+          
+        </div>
+      </section>
+    
+    </div>
+  );
+}
